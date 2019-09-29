ba71b7c6f98a27b47a48dd00b85df79fa59c8546
Sabrina Rocha <delrock876@gmail.com>
2019-09-28 19:33:25 -0700

    generate list function working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,15 +4,17 @@ document.addEventListener(`click`, event => {
   if (event.target.className === `moodBtn`) {
     mood = event.target.id
     console.log(mood)
+    let url = ` http://ws.audioscrobbler.com/2.0/?method=tag.getTopTracks&tag=${mood}&api_key=94d64342e57a2bf09615e32fc90ca58f&format=json`
+    fetch(url)
+      .then(r => r.json())
+      .then(data => {
+        data.tracks.track.forEach(track => {
+          console.log(track.name)
+
+        })
+      })
   }
 })
 
-let url = ` http://ws.audioscrobbler.com/2.0/?method=tag.getTopTracks&tag=${mood}&api_key=94d64342e57a2bf09615e32fc90ca58f&format=json`
-fetch(url)
-  .then(r => r.json())
-  .then(data => {
-    data.tracks.track.forEach(track => {
-      console.log(track.name)
 
-    })
-  })
+
